c7b56255c801122f6b4db2ab74eb1deb8c90d73e
Conner Simmons <cwsimmons149@gmail.com>
2016-03-02 22:34:05 -0500

    Full re-calibration is really close, if not complete.

--- SE532/src/lab4/EnExTee.java
+++ SE532/src/lab4/EnExTee.java
@@ -10,22 +10,27 @@ import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
-// import lejos.utility.Timer;
-// import lejos.utility.TimerListener;
+import lejos.utility.Timer;
+import lejos.utility.TimerListener;
 
-public class EnExTee {
+public class EnExTee implements TimerListener {
 	private static EV3UltrasonicSensor sensor;
 	private static Port port;
 	private static Port port2;
 	private static Port port3;
-	private static RegulatedMotor m1;
+	private static Port port4;
+	public static RegulatedMotor m1;
 	private static RegulatedMotor m2;
-	private static SampleProvider UV;
+	private static RegulatedMotor spinSensorMotor;
+	private static SampleProvider USsensor;
 	private static float[] sample;
+	private static Timer timer;
 
-	protected static int speed = 0;
+	protected static int angle = 0;
 	
 	protected static float distance = 0;
+	public static float minAngle = -1;
+	public static int tachoVal = 0;
 	
 	protected static int recalVal = 7;
 
@@ -39,18 +44,20 @@ public class EnExTee {
 		calibrate();
 
 		// loop guard and if must be the same
-		while (iterations <= 24) {
+		while (iterations <= 32) {
 			if (isObstructed == false) {
 				// Sends GoBot to final block
-				if (iterations == 24) {
-					speed = 1600;
-					rotate(m1,m2,speed);
+				if (iterations == 32) {
+					//angle = 1600;
+					angle = 1645;
+					rotate(m1,m2,angle);
 					iterations++;
 				}
 				// Sends GoBot to next block
 				else if (iterations % 2 == 0) {
-					speed = 780;
-					rotate(m1,m2,speed);
+					//angle = 780;
+					angle = 825;
+					rotate(m1,m2,angle);
 					iterations++;
 				}
 				// Turns GoBot and checks for re-calibrate 
@@ -59,8 +66,9 @@ public class EnExTee {
 						recalVal = recalVal + 8;
 						recalibrate();
 					}
-					speed = -165;
-					rotate(m1,m2,speed);
+					//angle = -165;
+					angle = -168;
+					rotate(m1,m2,angle);
 					iterations++;
 				}
 			}
@@ -68,7 +76,6 @@ public class EnExTee {
 
 		m1.close();
 		m2.close();
-
 	}
 
 	public static void setup() {
@@ -83,11 +90,15 @@ public class EnExTee {
 		port3 = LocalEV3.get().getPort("S1");
 		sensor = new EV3UltrasonicSensor(port3);
 		
-		UV = sensor.getMode("Distance");
-		sample = new float[UV.sampleSize()];
+		port4 = LocalEV3.get().getPort(MotorPort.C.getName());
+		spinSensorMotor = new EV3LargeRegulatedMotor(port4);
+		spinSensorMotor.setSpeed(300);
+		spinSensorMotor.setAcceleration(100);
+		
+		USsensor = sensor.getMode("Distance");
+		sample = new float[USsensor.sampleSize()];
 
-		// UVTimer listener = new UVTimer(port2, sensor);
-		// Timer timer = new Timer(1, listener);
+		timer = new Timer(1, new EnExTee());
 
 		LCD.drawString("Running.", 0, 0);
 
@@ -101,115 +112,101 @@ public class EnExTee {
 		m1.synchronizeWith(list);
 	}
 
-	public static void rotate(RegulatedMotor m1, RegulatedMotor m2, int speed) {
+	public static void rotate(RegulatedMotor m1, RegulatedMotor m2, int angle) {
 		m1.startSynchronization();
-		m1.rotate(speed, true);
-		m2.rotate(Math.abs(speed), true);
+		m1.rotate(angle, true);
+		m2.rotate(Math.abs(angle), true);
 		m1.endSynchronization();
 		m1.waitComplete();
 		m2.waitComplete();
 	}
 	
 	public static void calibrate() {
-		UV.fetchSample(sample, 0);
+		USsensor.fetchSample(sample, 0);
 		distance = sample[0];
 		Sound.beep();
 		LCD.drawString("Distance: " + distance, 0, 1);
 		Delay.msDelay(3000);
-		speed = -165;
-		rotate(m1,m2,speed);
+		angle = -165;
+		rotate(m1,m2,angle);
 	}
 	
 	public static void recalibrate() {
-		UV.fetchSample(sample, 0);
-		double difference = (double) distance - sample[0];
+		LCD.drawString("Recalibrating...", 0, 0);
+		
+		timer.start();
+		
+		USsensor.fetchSample(sample, 0);
+		spinSensorMotor.rotate(41);
+		spinSensorMotor.rotate(-82);
+		spinSensorMotor.rotate(41);
+		
+		timer.stop();
+		
+		//recal angle
+		LCD.drawString("Fixing the angle...", 0, 0);
+		LCD.drawString("minAngle: " + minAngle, 0, 1);
+		
+		float angleDiff = sample[0] - minAngle;
+		double angleRotations = angleDiff / 0.258;
+		int angleDegrees = ((int) (angleRotations * 360)) / -1;
+		LCD.drawString("angleDegrees: " + angleDegrees, 0, 2);
+		
+		if (angleDegrees >= 0) {
+			spinLeftWheel(angleDegrees);
+		} else {
+			spinRightWheel(angleDegrees);
+		}
+		
+		USsensor.fetchSample(sample, 0);
+		float difference = distance - sample[0];
 		Sound.beep();
-		LCD.drawString("Distance: " + difference, 0, 1);
+		LCD.drawString("Distance: " + difference, 0, 4);
 		Delay.msDelay(3000);
 		double rotations = difference / 0.258;
 		int degrees = ((int) (rotations * 360)) / -1;
-		LCD.drawString("Degrees: " + degrees, 0, 2);
+		LCD.drawString("Degrees: " + degrees, 0, 5);
 		rotateBack(m1,m2,degrees);
-		UV.fetchSample(sample, 0);
-		LCD.drawString("New distance: " + sample[0], 0, 3);
+		USsensor.fetchSample(sample, 0);
+		LCD.drawString("New distance: " + sample[0], 0, 6);
 	}
 	
-	public static void rotateBack(RegulatedMotor m1, RegulatedMotor m2, int speed) {
+	private static void spinLeftWheel(int angle) {
+		m2.setAcceleration(150);
+		m2.rotate(angle, true);
+		m2.waitComplete();
+		m2.setAcceleration(300);
+	}
+	
+	private static void spinRightWheel(int angle) {
+		m1.setAcceleration(150);
+		m1.rotate(angle, true);
+		m1.waitComplete();
+		m1.setAcceleration(300);
+	}
+	
+	public static void rotateBack(RegulatedMotor m1, RegulatedMotor m2, int angle) {
 		m1.setAcceleration(150);
 		m2.setAcceleration(150);
 		m1.startSynchronization();
-		m1.rotate(speed, true);
-		m2.rotate(speed, true);
+		m1.rotate(angle, true);
+		m2.rotate(angle, true);
 		m1.endSynchronization();
 		m1.waitComplete();
 		m2.waitComplete();
 		m1.setAcceleration(300);
 		m2.setAcceleration(300);
 	}
-
-	public static void interrupt() {
-		m1.startSynchronization();
-		m1.stop();
-		m2.stop();
-		m1.endSynchronization();
-		m1.waitComplete();
-		m2.waitComplete();
-		// m1Rotate = m1.getTachoCount();
-		// m2Rotate = m2.getTachoCount();
-	}
-
-	public static void restart() {
-		/*
-		m1.startSynchronization();
-		m1.rotate(speed - m1Rotate, true);
-		m2.rotate(Math.abs(speed) - m2Rotate, true);
-		m1.endSynchronization();
-		m1.waitComplete();
-		m2.waitComplete();
-		 */
-	}
-}
-/*
-class UVTimer implements TimerListener {
-
-	Port port;
-	EV3UltrasonicSensor s;
-	float[] sample;
-	SampleProvider UV;
-
-	int ticks = 0;
-
-	public UVTimer(Port port, EV3UltrasonicSensor s) {
-		this.port = port;
-		this.s = s;
-		UV = s.getMode("Distance");
-		s.enable();
-		sample = new float[UV.sampleSize()];
-	}
-
+	
 	@Override
 	public void timedOut() {
-		UV.fetchSample(sample, 0);
-		/*
-		ticks++;
+		USsensor.fetchSample(sample, 0);
 
-		if (sample[0] <= 0.15 && EnExTee.isObstructed == false) {
-			EnExTee.isObstructed = true;
-			EnExTee.interrupt();
+		if (minAngle == -1.0) {
+			minAngle = sample[0];
+		} else if (sample[0] < minAngle) {
+			minAngle = sample[0];
+			//tachoVal = m1.getTachoCount();
 		}
-		else { 
-			EnExTee.isObstructed = false; 
-			EnExTee.restart();
-		}
-
-		if (ticks % 100 == 0) {
-			LCD.drawString("Ticks: " + ticks, 0, 0); 
-		}
-
-		if (ticks % 500 == 0) {
-			LCD.drawString("Value: " + sample[0], 0, 1); 
-		}
-		
 	}
-
-} */
+}
