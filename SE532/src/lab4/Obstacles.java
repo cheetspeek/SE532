8618ed37fe80e90a35657a42df134866fe713629
Conner Simmons <cwsimmons149@gmail.com>
2016-03-05 17:30:03 -0500

    Final version of stopping for obstacles.

--- SE532/src/lab4/Obstacles.java
+++ SE532/src/lab4/Obstacles.java
@@ -21,14 +21,9 @@ public class Obstacles implements TimerListener {
 	private static SampleProvider sampleProvider;
 	private static float[] sample;
 	private static Timer timer;
-
 	protected static int angle = 0;
-	
 	protected static float distance = 0;
-	public static int tachoValMotor1 = 0;
-	public static int tachoValMotor2 = 0;
-
-	protected static Boolean isObstructed = false;
+	public static int turnAngleCounter = 0;
 
 	public static void main(String[] args) {
 		setup();
@@ -36,35 +31,34 @@ public class Obstacles implements TimerListener {
 		int iterations = 0;
 
 		timer.start();
-		
+
 		// loop guard and if must be the same
 		while (iterations <= 32) {
-			if (isObstructed == false) {
-				// Sends GoBot to final block
-				if (iterations == 32) {
-					//angle = 1600;
-					angle = 1690;
-					rotate(motor1,motor2,angle);
-					iterations++;
-				}
-				// Sends GoBot to next block
-				else if (iterations % 2 == 0) {
-					angle = 780;
-					//angle = 825;
-					rotate(motor1,motor2,angle);
-					iterations++;
-				}
-				// Turns GoBot and checks for re-calibrate 
-				else {
+			// Sends GoBot to final block
+			if (iterations == 32) {
+				angle = 1690;
+				rotate(motor1,motor2,angle);
+				iterations++;
+			}
+			// Sends GoBot to next block
+			else if (iterations % 2 == 0) {
+				angle = 780;
+				rotate(motor1,motor2,angle);
+				iterations++;
+			}
+			// Turns GoBot
+			else {
+				if (turnAngleCounter % 2 == 0) {
+					angle = -165;
+				} else {
 					angle = -166;
-					rotate(motor1,motor2,angle);
-					iterations++;
 				}
-			} else {
-				
+				turnAngleCounter++;
+				rotate(motor1,motor2,angle);
+				iterations++;
 			}
 		}
-		
+
 		timer.stop();
 
 		motor1.close();
@@ -82,7 +76,7 @@ public class Obstacles implements TimerListener {
 
 		sensorPort = LocalEV3.get().getPort("S1");
 		ultrasonicSensor = new EV3UltrasonicSensor(sensorPort);
-		
+
 		sampleProvider = ultrasonicSensor.getMode("Distance");
 		sample = new float[sampleProvider.sampleSize()];
 
@@ -91,7 +85,7 @@ public class Obstacles implements TimerListener {
 		LCD.drawString("Running.", 0, 0);
 
 		motor1.synchronizeWith(list);
-		
+
 		motor1.setSpeed(400);
 		motor2.setSpeed(400);
 		motor1.setAcceleration(150);
@@ -106,43 +100,29 @@ public class Obstacles implements TimerListener {
 		m1.waitComplete();
 		m2.waitComplete();
 	}
-	
+
 	public static void setMotorSpeedSlow(RegulatedMotor m1, RegulatedMotor m2) {
-		//m1.startSynchronization();
 		motor1.setSpeed(1);
 		motor2.setSpeed(1);
-		//m1.endSynchronization();
 	}
-	
+
 	public static void setMotorSpeedFast(RegulatedMotor m1, RegulatedMotor m2) {
-		//m1.startSynchronization();
 		motor1.setSpeed(400);
 		motor2.setSpeed(400);
-		//m1.endSynchronization();
 	}
-	
+
 	@Override
 	public void timedOut() {
 		sampleProvider.fetchSample(sample, 0);
-		
+
 		LCD.drawString("Distance: " + sample[0], 0, 0);
-		
+
 		if (sample[0] < 0.3) {
-			//motor1.setSpeed(1);
-			//motor2.setSpeed(1);
 			setMotorSpeedSlow(motor1,motor2);
 			LCD.drawString("Slow down.", 0, 3);
 		} else {
-			//motor1.setSpeed(500);
-			//motor2.setSpeed(500);
 			setMotorSpeedFast(motor1,motor2);
 			LCD.drawString("Speed up.", 0, 3);
 		}
-		
-		// LCD.drawString("TachoCount: " + motor1.getTachoCount(), 0, 1);
-		
-		//tachoValMotor1 = m1.getTachoCount();
-		//tachoValMotor2 = m2.getTachoCount();
-		
 	}
 }
